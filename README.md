0318adade45d28a7ecc882d89e581438a90849b4
Daniel Hotze <70265127+danielhotze@users.noreply.github.com>
2024-06-20 15:49:10 +0200

    add basic angular+electron setup

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # AISS-CV
 A PPE detection project using Jetson Nano devices for the university course "Artificial Intelligence in Service Systems - Computer Vision" at KIT Karlsruhe
+
+- [AISS-CV](#aiss-cv)
+  - [Intro](#intro)
+  - [Application](#application)
+    - [App Requirements](#app-requirements)
+  - [Jetson](#jetson)
+    - [Jetson Requirements](#jetson-requirements)
+
+
+## Intro
+Short Introduction.
+
+## Application
+General information about the application
+
+### App Requirements
+- Install Node.js and npm. (Recommendation: Use [nvm](https://www.freecodecamp.org/news/how-to-update-node-and-npm-to-the-latest-version/))
+- Install [MongoDB](https://www.mongodb.com/try/download/community) for the database
+- 
+
+## Jetson
+Content of section 3
+
+### Jetson Requirements
+What needs to be done to execute the model on a Jetson Nano Device?
---
+++ electron-app/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Database Setup:
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+// Electron Setup:
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // mainWindow.loadFile(path.join(__dirname, 'renderer', 'dist', 'index.html'));
+  mainWindow.loadFile('dist/electron-app/browser/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
