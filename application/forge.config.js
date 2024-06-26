94ecf2e92cc02b5a3389bcc75e744f5e7322f36e
Daniel Hotze <70265127+danielhotze@users.noreply.github.com>
2024-06-26 23:48:14 +0200

    - add electron-forge for packaging application - add some skeleton python code for the jetson

---
+++ application/forge.config.js
@@ -0,0 +1,59 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require('path');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: './public/assets/icons/favicon'
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        iconUrl: path.join(__dirname, 'public', 'assets', 'icons', 'favicon'),
+        setupIcon: './public/assets/icons/favicon.ico'
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          icon: './public/assets/icons/favicon.png'
+        }
+      },
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        icon: './public/assets/icons/favicon.icns'
+      }
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
---
+++ jetson/main.py
@@ -0,0 +1,62 @@
+import cv2
+import requests
+import time
+import uuid
+#import socket
+# DH: Rough sketch of how I envision the communication with the server.
+# Real implementation could use cv2 to get the images in an image feed and somewhere in there run the incident processing
+
+######################################## VARIABLES ########################################
+# Server Settings
+SERVER_IP = 'localhost' # add flask API through which the server can tell the device its' IP
+SERVER_API = 'http://' + SERVER_IP + ':3000/api'
+INCIDENT_CREATE_API = SERVER_API + '/incidents'
+IMAGE_UPLOAD_API = SERVER_API + '/incidents/image'
+
+# Device Settings
+#hostname = socket.gethostname()
+#device_ip = socket.gethostbyname(hostname) # option for dynamically getting the IP-Address
+DEVICE_ID = 'device1' # how do we best configure this? Probably some setup over flask API?
+
+# Incident Variables
+MAX_UNDETECTED_FRAMES = 5
+SKIP_IMAGE_UPLOAD_MOD = 5 # uploading every frame might be too much - only upload every x frames
+incident_uuid = None
+incident_active = False
+incident_type = None
+frames_without_detection = 0
+
+
+######################################## LOGIC ########################################
+# Server requests
+def send_create_incident(id, timestamp):
+  i = 1
+
+def send_update_incident():
+  i = 1
+
+def send_upload_image():
+  i = 1
+# Here Camera Logic - Get frame from camera (use cv2 or something)
+def get_frame_from_camera():
+  return
+
+# Here Detection Logic - Use the model to detect ppe violations
+def detect_objects(frame):
+  return
+
+# Here Main Process (rough sketch - subject to change)
+while True:
+  frame = get_frame_from_camera() 
+  detection_result = detect_objects(frame)
+
+  if detection_result: # if some violation was detected
+    # for new incident or different detection type, create new incident?
+    if (not incident_active) or detection_result != incident_type:
+      incident_uuid = str(uuid.uuid4())
+      timestamp = time.time()
+      incident_active = True
+      incident_type = detection_result
+    else:
+      timestamp = time.time()
+
