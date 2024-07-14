3cf96dffba2275a1354b1bd49f91241c75eb2d06
Daniel Hotze <70265127+danielhotze@users.noreply.github.com>
2024-07-15 00:00:03 +0200

    add more devices content

--- application/src/app/pages/devices/components/devices-add/devices-add.component.ts
+++ application/src/app/pages/devices/components/devices-add/devices-add.component.ts
@@ -2,18 +2,25 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Device } from '../../../../core/models/device.model';
 import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { DevicesService } from '../../../../core/services/devices.service';
 
 @Component({
   selector: 'app-devices-add',
   standalone: true,
-  imports: [MatIconModule, FormsModule],
+  imports: [MatIconModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './devices-add.component.html',
   styleUrl: './devices-add.component.css'
 })
 export class DevicesAddComponent {
   @Input() newDevice: Device = this.generateDeviceSkeleton();
   @Output() addDevice: EventEmitter<Device> = new EventEmitter();
-  public isValid = this.checkValidity();
+  public validationError = '';
+
+  constructor(
+    private deviceService: DevicesService
+  ) {}
 
   private generateDeviceSkeleton(): Device {
     return {
@@ -29,14 +36,26 @@ export class DevicesAddComponent {
     const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
     const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
     if (!ipv4Pattern.test(this.newDevice.ip) && !ipv6Pattern.test(this.newDevice.ip)) {
-      this.isValid = false;
+      this.validationError = 'Device IP is not in valid ipv4/ipv6 format.';
       return false;
     }
-    if (!(this.newDevice.name.length > 0) || !(this.newDevice.location.length > 0)) {
-      this.isValid = false;
+    if (this.deviceService.devices.find(d => d.ip === this.newDevice.ip)) {
+      this.validationError = 'A device with this IP address already exists.';
       return false;
     }
-    this.isValid = true;
+    if (this.deviceService.devices.find(d => d.name === this.newDevice.name)) {
+      this.validationError = 'A device with this name already exists.';
+      return false;
+    }
+    if (!(this.newDevice.name.length > 0)) {
+      this.validationError = 'Device needs a name.';
+      return false;
+    }
+    if (!(this.newDevice.location.length > 0)) {
+      this.validationError = 'Device needs a location.';
+      return false;
+    }
+    this.validationError = '';
     return true;
   }
 
@@ -46,6 +65,10 @@ export class DevicesAddComponent {
       return;
     }
     this.addDevice.emit({...this.newDevice});
+    this.newDevice = this.generateDeviceSkeleton();
+  }
+
+  public resetNewDevice() {
     this.newDevice = this.generateDeviceSkeleton();
   }
 
